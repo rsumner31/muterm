4afd0e0ad755c771a695bca1c9a28bf66eada780
Robert Sumner Jr <rsumner31@gmail.com>
2018-03-29 22:30:28 -0500

    LazSerialPort.o

--- components/lnet/lazaruspackage/lnetbase.pas
+++ components/lnet/lazaruspackage/lnetbase.pas
@@ -1,17 +1,41 @@
 { Tento súbor bol automaticky vytvorený Lazarom. Prosím neurpavovať!
   Tento zdrojový kód je použitý len na preloženie a nainštalovanie tohoto 
     balíčka.
- }
-
-unit lnetbase; 
+is file { This file was automatically created by Lazarus. Do not edit!
+Tento zdrojový kód je použitý len na preloženie a nainštalovanie tohoto balíčka.
+>>>>>>>-beb31d1
 
 interface
 
 uses
-    lNet, lEvents, lCommon, lFTP, lsmtp, lTelnet, lhttp, lwebserver, 
+<<<<<<< HEAD
+    lNe    lNet, lEvents, lCommon, lFTP, lsmtp, lTelnet, lhttp, lwebserver, 
   lMimeWrapper, lHTTPUtil, lControlStack, lMimeStreams, lMimeTypes, lProcess, 
   lSpawnFCGI, lStrBuffer, ltimer, lNetSSL, lThreadEvents, fastcgi_base;
+>>>>>>>+HEAD
+nts, lCo  lNet, lEvents, lCommon, lFTP, lsmtp, lTelnet, lhttp, lwebserver, 
+    lMimeWrapper, lHTTPUtil, lControlStack, lMimeStreams, lMimeTypes, fastcgi, 
+    lfastcgi, lProcess, lSpawnFCGI, lStrBuffer, ltimer, lNetSSL; 
+>>>>>>>-beb31d1
+.
+<<<<<<<+HEAD
+{ Tento súbor bol automaticky vytvorený Lazarom. Prosím neurpavovať!
+  Tento zdrojový kód je použitý len na preloženie a nainštalovanie tohoto 
+    balíčka.
+is file { This file was automatically created by Lazarus. Do not edit!
+Tento zdrojový kód je použitý len na preloženie a nainštalovanie tohoto balíčka.
+>>>>>>>-beb31d1
 
-implementation
+interface
 
-end.
+uses
+<<<<<<< HEAD
+    lNe    lNet, lEvents, lCommon, lFTP, lsmtp, lTelnet, lhttp, lwebserver, 
+  lMimeWrapper, lHTTPUtil, lControlStack, lMimeStreams, lMimeTypes, lProcess, 
+  lSpawnFCGI, lStrBuffer, ltimer, lNetSSL, lThreadEvents, fastcgi_base;
+>>>>>>>+HEAD
+nts, lCo  lNet, lEvents, lCommon, lFTP, lsmtp, lTelnet, lhttp, lwebserver, 
+    lMimeWrapper, lHTTPUtil, lControlStack, lMimeStreams, lMimeTypes, fastcgi, 
+    lfastcgi, lProcess, lSpawnFCGI, lStrBuffer, ltimer, lNetSSL; 
+>>>>>>>-beb31d1
+.
